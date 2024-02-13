96af7f7468baa4196681829441b3779d7e92f7ca
Howard Lince III <howard.lince@infinit-o.com>
2024-02-13 00:44:27 -0300

    init

---
+++ private_dot_config/private_fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    fish_vi_key_bindings
+    fish_add_path /usr/local/go/bin
+    fish_add_path ~/go/bin/
+    alias nvim="~/.local/bin/nvim.appimage"
+    # Commands to run in interactive sessions can go here
+end
