6822ce353a8b3267e312434a932ad5aee3bf2d85
Howard Lince III <howard.lince@infinit-o.com>
2024-02-16 22:45:02 -0300

    better cat replacement strategy

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -3,8 +3,17 @@ if status is-interactive
     fish_add_path /usr/local/go/bin
     fish_add_path ~/go/bin/
     alias nvim="~/.local/bin/nvim.appimage"
-    alias cat="batcat"
     # Commands to run in interactive sessions can go here
 end
 
 abbr -a ccd chezmoi cd
+
+function cat
+    if type -q batcat
+        batcat $argv
+    else
+        echo "Warning: batcat is not installed, using standard cat."
+        command cat $argv
+    end
+end
+
